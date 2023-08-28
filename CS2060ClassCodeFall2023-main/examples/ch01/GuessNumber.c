a6501e6b2824b67ca9cb06d86c1418d90f17b2ee
DingDongReallyFast <jhpipboy@gmail.com>
2023-08-28 16:36:14 -0600

    Update GuessNumber.c

--- CS2060ClassCodeFall2023-main/examples/ch01/GuessNumber.c
+++ CS2060ClassCodeFall2023-main/examples/ch01/GuessNumber.c
@@ -1,58 +1,58 @@
 // Randomly generate numbers between a min and max for user to guess.
 
 
-//
+// Header files
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-//
+// Define constants
 #define MIN 1
 #define MAX 1000
 
-//
+//Initialize and define functions
 void guessGame(void); 
 bool isCorrect(int guess, int answer); 
 
 int main(void) {
-    //
+    // Gain random number for rand
    srand(time(0)); 
-   puts("Hello Name");
-   //
+   
+   // Start guessing game
    guessGame();
 } // end main
 
 
 
-// 
+// Start guessGame
 void guessGame(void) {
     
-    //
+    // Intialize variables
    int response =0;
    int guess = 0;
 
-   // 
+   // Loop for user inputs
    do {
 
-      // 
+      // Assign answer value 
       int answer = 1 + rand() % 1000;
 
-      //
+      // Explain limits of game
       printf("I have a number between %d and %d.\n", MIN, MAX);
 
-      // 
+      // Prompt user for input
       puts("Can you guess my number?\n" 
            "Please type your first guess.");
 
-      //
+      // Print question mark
       printf("%s", "? ");
       
 
-      //
+      // Scan and place user input into guess
       scanf("%d", &guess);
 
-      // 
+      // Scan user input again if false
       while (!isCorrect(guess, answer)) {
          scanf("%d", &guess);
       }
@@ -67,18 +67,18 @@ void guessGame(void) {
    } while (response == 1);
 } // end function guessGame
 
-// 
+// Check for correct answer
 bool isCorrect(int guess, int answer) {
 
-    //
+    // Intialize correct
     bool correct = false;
 
-   // 
+   // Check guess for same value
    if (guess == answer) {
       correct = true;
    }
 
-   // 
+   // Give hint to user 
    if (guess < answer) {
       printf( "%s", "Too low. Try again.\n? " );
    }
