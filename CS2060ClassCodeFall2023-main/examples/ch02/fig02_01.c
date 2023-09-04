133fd2f2b661e27102db7d874da6167758a2bfa9
DingDongReallyFast <jhpipboy@gmail.com>
2023-09-04 15:03:30 -0600

    Update fig02_01.c

--- CS2060ClassCodeFall2023-main/examples/ch02/fig02_01.c
+++ CS2060ClassCodeFall2023-main/examples/ch02/fig02_01.c
@@ -1,10 +1,83 @@
 // fig02_01.c
 // A first program in C.
 #include <stdio.h>
+#include <stdbool.h>
 
 // function main begins program execution 
 int main(void) {
+   // Initialize variables
+   int length = 0;
+   int width = 0;
+   int area = 0;
+   double averageValue = 0;
+   int scanfRef = 0;
+   bool checkScan = false;
+
+   // Print welcome
    printf("Welcome to C!\n");
+
+   // Prompt user for length
+   puts("Enter the length:");
+
+   // Loop each time user inputs invalid value
+   do {
+	   // Allow user to input
+	   scanfRef = scanf("%d", &length);
+	   
+	   // Referesh input buffer
+	   while ((getchar()) != '\n');
+
+		// Check if it is valid user input
+	    // Can leave loop
+		if (scanfRef != 0) {
+			checkScan = true;
+		}
+		// Print error message then loop again
+		else {
+			puts("Not a valid input, enter again:");
+		}
+   } while(!checkScan);
+
+   // Reset checking values
+   checkScan = false;
+   scanfRef = 0;
+
+   // Prompt user for width
+   puts("Enter the width:");
+
+  // Loop each time user inputs invalid value
+   do {
+	   // Allow user to input
+	   scanfRef = scanf("%d", &width);
+
+	   // Referesh input buffer
+	   while ((getchar()) != '\n');
+
+	   // Check if it is valid user input
+	   // Can leave loop
+	   if (scanfRef != 0) {
+		   checkScan = true;
+	   }
+	   // Print error message then loop again
+	   else {
+		   puts("Not a valid input, enter again:");
+	   }
+   } while (!checkScan);
+
+   // Calculate area and average value
+   area = length * width;
+
+   // Print out inputs and area
+   printf("Length: %d Width: %d Area: %d", length, width, area);
+
+   // Calculate average value
+   averageValue = (double) (length + width) / 2;
+
+   // Print average value
+   printf(" Average value: %.1f", averageValue);
+
+   // End main function
+   return 0;
 } // end function main 
 
 
