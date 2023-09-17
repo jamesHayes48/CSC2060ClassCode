cdb25f2f70151e3721ca43548b24a2fb4dde9d5f
DingDongReallyFast <jhpipboy@gmail.com>
2023-09-16 20:04:59 -0600

    Update hayesJ_ITER01.c

--- CS2060ClassCodeFall2023-main/examples/Homework/hayesJ_ITER01.c
+++ CS2060ClassCodeFall2023-main/examples/Homework/hayesJ_ITER01.c
@@ -1,19 +1,39 @@
 // Header files
 #include <stdio.h>
+#include <stdlib.h>
 
 // Constants
-# define const SENTINEL_NEG1 -1
-# define int unsigned const MIN_RENTAL_NIGHTS 1
-# define unsigned int const MAX_RENTAL_NIGHTS 14
-# define unsigned int const INTERVAL_1_NIGHTS 3
-# define unsigned int const INTERVAL_2_NIGHTS 6
-# define double const RENTAL_RATE 400
-# define double const DISCOUNT 50
+#define const SENTINEL_NEG1 -1
+#define int unsigned const MIN_RENTAL_NIGHTS 1
+#define unsigned int const MAX_RENTAL_NIGHTS 14
+#define unsigned int const INTERVAL_1_NIGHTS 3
+#define unsigned int const INTERVAL_2_NIGHTS 6
+#define double const RENTAL_RATE 400
+#define double const DISCOUNT 50
 
 // Intialize and declare functions
-void printRentalPropertyInfo(unsigned int minNights, unsigned int maxNights, 
-	unsigned int interval1Nights, unsigned int interval2Nights, double rate, double discount);
+void printRentalPropertyInfo(unsigned int minNights, (unsigned) int maxNights, unsigned int interval1Nights, unsigned int interval2Nights, double rate, double discount);
 int getValidInt(int min, int max);
-double calculateCharges(unsigned int nights, unsigned int interval1Nights, 
-	unsigned int interval2Nighs, double);
-void printNightsCharges(unsigned int nights, double charges);
+double calculateCharges(unsigned int nights, unsigned int interval1Nights, unsigned int interval2Nighs, double);
+void printNightsCharges((unsigned) int nights, double charges);
+
+// Main function
+int main(void) {
+	printf("Working");
+}
+
+void printRentalPropertyInfo(unsigned int minNights, unsigned int maxNights, unsigned int interval1Nights, unsigned int interval2Nights, double rate, double discount) {
+
+}
+
+int getValidInt(int min, int max) {
+
+}
+
+double calculateCharges(unsigned int nights, unsigned int interval1Nights, unsigned int interval2Nighs, double) {
+
+}
+
+void printNightsCharges(unsigned int nights, double charges) {
+
+}
