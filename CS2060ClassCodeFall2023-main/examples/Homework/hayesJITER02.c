6cac7cd306e5d30731a56db129479f9c2f3ab018
DingDongReallyFast <jhpipboy@gmail.com>
2023-10-29 19:34:24 -0600

    It can get nights charges, but not ratings
    Work on improving main structure and make sure it can get survey ratings.

--- CS2060ClassCodeFall2023-main/examples/Homework/hayesJITER02.c
+++ CS2060ClassCodeFall2023-main/examples/Homework/hayesJITER02.c
@@ -16,7 +16,7 @@
 #define CORRECT_ID "id1"
 #define CORRECT_PASSCODE "ABCD"
 #define LOGIN_MAX_ATTEMPTS 3
-#define SENTINAL_NEG1 -1
+#define SENTINEL_NEG1 -1
 
 // Rental property constant ranges
 #define MIN_RENTAL_NIGHTS 1
@@ -45,17 +45,39 @@ void removeNewLineChar(char* string);
 void setupProperty(Property* propertyPtr, const int minNights, const int maxNights, const int minRate, const int maxRate);
 int getValidInt(const int min, const int max);
 void scanInt(char* input, int* result);
+void rentalMode(Property* propertyPtr, int sentinel, const char* correctID, const char* correctPasscode, const int unsigned maxAttempts);
 void displayPropertyInfo(Property* propertyPtr);
+int getValidSentinel(const int min, const int max, const int sentinel);
+double calculateCharges(unsigned int nights, unsigned int interval1, unsigned int interval2, double rate, double discount);
+void printNightsCharges(unsigned int nights, double charges);
+
+// Print survey information
+void printSurveyInformation(int minRating, int maxRating, const char* categories[], size_t totalCategories);
+
+// Print categories for rating
+void printCategories(const char* categories[], size_t totalCategories);
+
+// Loop and add input into 2D array
+void getRatings(int minRating, int maxRating, int survey[][RENTER_SURVEY_CATEGORIES], const char* categories[], size_t totalUsers, size_t totalCategories);
+
+// Get valid input
+int getValidInt(int minRating, int maxRating);
+
+// Print the results of the survey
+void printSurveyResults(const int survey[][RENTER_SURVEY_CATEGORIES], size_t totalUsers, size_t totalCategories);
+
+// Calculate the average for each category
+void calculateCategoryAverages(double averages[], int survey[][RENTER_SURVEY_CATEGORIES], size_t totalUsers, size_t totalCategories);
+
+// Print the averages for each category
+void printCategoryData(const double averages[], size_t totalUsers, size_t totalCategories);
 
 int main(void) {
 	Property property1;
 	bool ownerLogin = login(CORRECT_ID, CORRECT_PASSCODE, LOGIN_MAX_ATTEMPTS);
-	if (ownerLogin == true) {
+	while (ownerLogin == true) {
 		setupProperty(&property1, MIN_RENTAL_NIGHTS, MAX_RENTAL_NIGHTS, MIN_RATE, MAX_RATE);
-		displayPropertyInfo(&property1);
-	}
-	else {
-		puts("Goodbye");
+		rentalMode(&property1, SENTINEL_NEG1, CORRECT_ID, CORRECT_PASSCODE, LOGIN_MAX_ATTEMPTS);
 	}
 }
 
@@ -111,7 +133,9 @@ void setupProperty(Property* propertyPtr, const int minNights, const int maxNigh
 	propertyPtr->totalCharge = 0;
 	propertyPtr->totalNights = 0;
 	propertyPtr->totalRenters = 0;
-	
+	//propertyPtr->averages[] = {0};
+	//propertyPtr->survey[] = {0};
+
 	printf("%s", "Enter name of property: \n");
 	fgets(propertyPtr->propertyName, STRING_LENGTH, stdin);
 
@@ -141,7 +165,7 @@ int getValidInt(const int min, const int max) {
 		removeNewLineChar(inputStr);
 		scanInt(inputStr, &result);
 
-		if ((result < max) && (result > min)) {
+		if ((result <= max) && (result >= min)) {
 			isValid = true;
 		}
 		else {
@@ -157,8 +181,97 @@ void scanInt(char* input, int* result) {
 	*result = (int)intCheck;
 }
 
+void rentalMode(Property* propertyPtr, int sentinel, const char* correctID, const char* correctPasscode, const int unsigned maxAttempts) {
+	bool validSentinel = false;
+	int userNights = 0;
+	double currentCharge = 0;
+
+	do {
+		displayPropertyInfo(propertyPtr);
+		if (propertyPtr->averages[0] != 0) {
+			puts("Teehee");
+		}
+		puts("Enter number of nights");
+		userNights = getValidSentinel(MIN_RENTAL_NIGHTS, MAX_RENTAL_NIGHTS, sentinel);
+		if (userNights == sentinel) {
+			login(correctID, correctPasscode, maxAttempts);
+			validSentinel = true;
+		}
+		else {
+			propertyPtr->totalNights += userNights;
+			currentCharge = calculateCharges(userNights, (propertyPtr->interval1), (propertyPtr->interval2), (propertyPtr->rentalRate), (propertyPtr->discount));
+			propertyPtr->totalCharge += currentCharge;
+			printNightsCharges(userNights, currentCharge);
+		}
+	} while (validSentinel == false);
+}
+
 void displayPropertyInfo(Property* propertyPtr) {
 	printf("The first interval is %d, the second is %d\n", (propertyPtr->interval1), (propertyPtr->interval2));
 	printf("The rental rate is %d and the discount is %d\n", (propertyPtr->rentalRate), (propertyPtr->discount));
 	printf("The name of the property is %s, the location is %s\n", (propertyPtr->propertyName), (propertyPtr->propertyLocation));
 }
+
+int getValidSentinel(const int min, const int max, const int sentinel) {
+	bool isValid = false;
+	char inputStr[STRING_LENGTH];
+	int result = 0;
+
+	while (isValid == false) {
+		fgets(inputStr, STRING_LENGTH, stdin);
+		removeNewLineChar(inputStr);
+		scanInt(inputStr, &result);
+
+		if ((result <= max) && (result >= min)) {
+			isValid = true;
+		}
+		else if (result == sentinel) {
+			isValid = true;
+		}
+		else {
+			puts("Invalid input, please another number: ");
+		}
+	}
+	return result;
+}
+
+double calculateCharges(unsigned int nights, unsigned int interval1, unsigned int interval2, double rate, double discount) {
+	// Intialize count
+	int unsigned count = 1;
+
+	// Intialize charges
+	double charges = 0;
+
+	// Loop until count is greater than number of nights
+	while (!(count > nights)) {
+
+		// Add charge each time
+		charges += rate;
+
+		// If count is greater than 1st interval apply discount
+		if (count > interval1) {
+			charges -= discount;
+
+			// If count is greatert than 2nd interval apply discount
+			if (count > interval2) {
+				charges -= discount;
+			}
+		}
+
+		// Iterate count
+		count++;
+	}
+	return charges;
+}
+
+void printNightsCharges(unsigned int nights, double charges) {
+	// If no rentals were made, then print then no rentals were made
+	if (nights == charges) {
+		puts("\nThere were no rentals\n");
+	}
+	//  If there were inputs, output the result
+	else {
+		printf("\nNights: %d Charge: $%.2f\n", nights, charges);
+	}
+}
+
